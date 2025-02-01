2152d1be398177ddd58b3ef09db28a2af7fd3e09
sinner <sinner885@gmail.com>
2025-02-01 23:02:35 +0200

    wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,10 +10,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from environ import Env
+
+env = Env()
+env.read_env()
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infopogreb.settings')
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "config.setting")
+    "DJANGO_SETTINGS_MODULE", env.str("DJANGO_SETTINGS_MODULE", "config.setting")
 )
 
 application = get_wsgi_application()
